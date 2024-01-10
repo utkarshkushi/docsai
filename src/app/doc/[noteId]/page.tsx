e9f2f81bad205323da5a85f096f77f1d33d5545d
utkarshkuhsi <utkarshsuneela@gmail.com>
2024-01-10 22:37:04 +0530

    adding doc page ui tiptap editor

--- src/app/doc/[noteId]/page.tsx
+++ src/app/doc/[noteId]/page.tsx
@@ -1,4 +1,6 @@
+import TipTapEditor from '@/components/TipTapEditor'
 import { Button } from '@/components/ui/button'
+import { clerk } from '@/lib/clerk-server'
 import { db } from '@/lib/db'
 import { $notes } from '@/lib/db/schema'
 import { auth } from '@clerk/nextjs'
@@ -15,9 +17,13 @@ type Props = {
 
 const DocPage = async ({ params: { noteId } }: Props) => {
     const { userId } = await auth();
+
+
     if (!userId) {
         return redirect("/dashboard");
     }
+
+    const user = await clerk.users.getUser(userId);
 
     const notes = await db
         .select()
@@ -32,13 +38,26 @@ const DocPage = async ({ params: { noteId } }: Props) => {
     return (
         <div className="min-h-screen grainy p-8">
             <div className="max-w-4xl mx-auto">
-            <div className='border shadow-xl border-stone-200 rounded-lg p-4 flex items-center'>
-            <Link href="/dashboard">
-            <Button className="bg-green-600" size="sm">
-              Back
-            </Button>
-          </Link>
-            </div>
+                <div className='border shadow-xl border-stone-200 rounded-lg p-4 flex items-center'>
+                    <Link href="/dashboard">
+                        <Button className="bg-green-600" size="sm">
+                            Back
+                        </Button>
+                    </Link>
+                    <div className="w-3"></div>
+                    <span className="font-semibold">
+                        {user.firstName} {user.lastName}
+                    </span>
+                    <span className="inline-block mx-1">/</span>
+                    <span className="text-stone-500 font-semibold">{note.name}</span>
+                    <div className="ml-auto">
+                        {/* <DeleteButton noteId={note.id} /> */}DeleteButton
+                    </div>
+                </div>
+                <div className="h-4"></div>
+                <div className="border-stone-200 shadow-xl border rounded-lg px-16 py-8 w-full">
+                    <TipTapEditor />
+                </div>
             </div>
         </div>
     )
